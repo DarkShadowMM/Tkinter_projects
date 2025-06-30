0d68a891a3c1416b126697b94c2090c4ff8c1263
MuradM <muradmirzeyev0407@gmail.com>
2025-06-30 12:43:17 +0400

    Add files via upload

---
+++ Paint_gemini.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+
+def start_drawing(event):
+    global last_x, last_y
+    last_x, last_y = event.x, event.y
+
+def draw(event):
+    global last_x, last_y
+    canvas.create_line(last_x, last_y, event.x, event.y, width=40)
+    last_x, last_y = event.x, event.y
+
+def stop_drawing(event):
+    pass
+
+root = tk.Tk()
+root.title("Simple Paint")
+
+canvas = tk.Canvas(root, bg="white", width=600, height=400)
+canvas.pack()
+
+canvas.bind("<Button-1>", start_drawing)
+canvas.bind("<B1-Motion>", draw)
+canvas.bind("<ButtonRelease-1>", stop_drawing)
+
+last_x, last_y = None, None
+
+root.mainloop()
